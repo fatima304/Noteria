63222ffe561b32dfcd20c7693f2fb89f1e7c29e4
fatmaatef4 <fatmaatef015@gmailcom>
2023-11-05 15:01:54 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const NotesApp(),
+  );
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 89, 66, 117),
+        ),
+      ),
+    );
+  }
+}
